ad535e523dedddc53fe58f8209d2ed6c3625d39d
Michael Felipe Rondón Acosta <mfrondona@unal.edu.co>
2017-05-15 23:27:40 -0500

    Update Ejecutar.bat

--- timestampclient/Ejecutar.bat
+++ timestampclient/Ejecutar.bat
@@ -6,13 +6,3 @@ echo ---------------------------------------
 echo ---------------------------------------
 pause
 docker build -t timestamp-client .
-echo ---------------------------------------
-echo ---------------------------------------
-echo Executing timestamp-client image:
-echo ---------------------------------------
-echo ---------------------------------------
-pause
-REM docker login
-REM docker tag timestamp-client mfrondon/timestamp-client:1.0
-REM docker push mfrondon/timestamp-client:1.0
-docker run -p 4001:8080 mfrondon/timestamp-client:1.0
